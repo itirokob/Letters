a4f356dcb3b66b38a1bdbb5e23d0c6ae4ef18bb9
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-07-19 12:17:00 -0300

    Custom animated initial screen

--- Letter/Controller/InitialScreenViewController.swift
+++ Letter/Controller/InitialScreenViewController.swift
@@ -11,14 +11,29 @@ import UIKit
 class InitialScreenViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
+    var customView: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadCustomView()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        customView?.removeFromSuperview()
+    }
+    
+    private func loadCustomView() {
         // Load random screen
         let screen: InitialScreen = InitialScreenReference.getRandomScreen()
         if let view = UINib(nibName: screen.nibName, bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? UIView {
             self.view.addSubview(view)
             self.view.sendSubview(toBack: view)
+            customView = view
         }
         self.button.setImage(UIImage(named: screen.buttonName), for: .normal)
     }
