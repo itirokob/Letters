87c25705515dfccac6b205a235fe11e86f93a1ba
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-07-20 17:31:35 -0300

    Bear

--- Letter/Model/ObjectReference.swift
+++ Letter/Model/ObjectReference.swift
@@ -21,7 +21,8 @@ class ObjectReference: NSObject {
         ObjectModel(id: "1", name: "aranha", filename:"spider", image: #imageLiteral(resourceName: "Elephant"), hiddenImage: #imageLiteral(resourceName: "hiddenElephant")),
         ObjectModel(id: "2", name: "alce", filename: "deer", image: #imageLiteral(resourceName: "deer"), hiddenImage: #imageLiteral(resourceName: "hiddenDeer")),
         ObjectModel(id: "3", name: "robô", filename: "robot", image: #imageLiteral(resourceName: "robot"), hiddenImage: #imageLiteral(resourceName: "hiddenRobot")),
-        ObjectModel(id: "4", name: "cachorro", filename: "dog", image: #imageLiteral(resourceName: "dog"), hiddenImage: #imageLiteral(resourceName: "hiddenDog"))
+        ObjectModel(id: "4", name: "cachorro", filename: "dog", image: #imageLiteral(resourceName: "dog"), hiddenImage: #imageLiteral(resourceName: "hiddenDog")),
+        ObjectModel(id: "5", name: "urso", filename: "bear", image: #imageLiteral(resourceName: "bear"), hiddenImage: #imageLiteral(resourceName: "hiddenBear"))
     ]
     
     private override init() {
