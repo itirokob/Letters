88524b3b5251508a106656698443e492b8303df0
Bianca Itiroko <itirokob@gmail.com>
2018-07-20 09:25:02 -0300

    Separei a extensao da matrix 4x4 num arquivo

--- Letter/Controller/ViewController.swift
+++ Letter/Controller/ViewController.swift
@@ -189,10 +189,3 @@ class ViewController: UIViewController, ARSCNViewDelegate, GamePresenterDelegate
         // Reset tracking and/or remove existing anchors if consistent tracking is required
     }
 }
-
-extension float4x4 {
-    var translation: float3 {
-        let translation = self.columns.3
-        return float3(translation.x, translation.y, translation.z)
-    }
-}
